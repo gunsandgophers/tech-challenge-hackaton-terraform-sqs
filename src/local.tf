7e2ed6b74efaad94ec077a7a84f246d345404d48
Rafael Vinicius <rafael.sampaio@loft.com.br>
2025-02-02 12:00:25 -0300

    fix: sqs

--- src/local.tf
+++ src/local.tf
@@ -1,13 +1,3 @@
 locals {
-  name   = "tech-challenge-hackaton-rds"
   region = "us-east-1"
-
-  engine         = "postgres"
-  engine_version = "16"
-  family         = "postgres16"
-  instance_class = "db.t3.micro"
-
-  db_name     = "tech_challenge_hackaton"
-  db_username = "tech_challenge_hackaton"
-
 }
---
+++ src/process-video-queue.tf
@@ -0,0 +1,51 @@
+resource "aws_sqs_queue" "process_video_queue" {
+  name                       = "process-video-queue"
+  max_message_size           = 2048
+  message_retention_seconds  = 86400
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.process_video_queue_deadletter.arn
+    maxReceiveCount     = 10
+  })
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+data "aws_iam_policy_document" "process_video_queue_policy" {
+  statement {
+    sid    = "shsqsstatement"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage"
+    ]
+    resources = [
+      aws_sqs_queue.process_video_queue.arn
+    ]
+  }
+}
+
+resource "aws_sqs_queue_policy" "process_video_queue_policy" {
+  queue_url = aws_sqs_queue.process_video_queue.id
+  policy    = data.aws_iam_policy_document.process_video_queue_policy.json
+}
+
+resource "aws_sqs_queue" "process_video_queue_deadletter" {
+  name = "process-video-queue-dlq"
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "process_video_queue_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.process_video_queue_deadletter.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.process_video_queue.arn]
+  })
+}
